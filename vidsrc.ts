892bb5ecdbd4811a852b84b9de42837d53fcaa86
cool-guy-dev <116984615+cool-dev-guy@users.noreply.github.com>
2024-09-19 10:22:35 +0530

    Update vidsrc.ts

--- vidsrc.ts
+++ vidsrc.ts
@@ -8,7 +8,7 @@ NOTES:
 import cheerio from "cheerio"; // FOR NODE.JS
 import cheerio from "npm:cheerio"; // FOR DENO
 */
-import cheerio from "npm:cheerio";
+import cheerio from "cheerio";
 import { decrypt } from "./helpers/decoder.ts";
 
 const BASEDOM = "https://whisperingauroras.com";
